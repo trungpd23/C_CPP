59542ab3b5764b67be88c7f320278cf91ae89414
trungpd23 <phamdinhtrung102@gmail.com>
2026-02-27 23:29:31 +0700

    update

--- Array_Pointers_References/example.cpp
+++ Array_Pointers_References/example.cpp
@@ -5,9 +5,10 @@ void sensorDataManagement(){
     int size = 5;
     float temperature[5] = {23.5, 24.1, 22.8, 25.0, 23.9};
     float sum = 0.0;
+    std::cout << "Du lieu cam bien: ";
     for(int i = 0; i < size; i++) {
         sum += temperature[i];
-        std::cout << "Du lieu cam bien: " << temperature[i] << " ";
+        std::cout << temperature[i] << " ";
         if(i == size - 1) std::cout << std::endl;
     }
 
